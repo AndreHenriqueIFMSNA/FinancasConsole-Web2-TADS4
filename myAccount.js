8d83c1c9dd5dbea14a8af43a6d1c272054374e2f
AndreHenriqueIFMSNA <99371362+AndreHenriqueIFMSNA@users.noreply.github.com>
2024-03-11 21:52:07 -0400

    Update myAccount.js
    Finalizado

--- myAccount.js
+++ myAccount.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const chalk = require("chalk");
-const inquirer = require("inquirer");
+const chalk = require('chalk');
+const inquirer = require('inquirer');
 
 var caminhoUsuario;
 var usuario;
@@ -10,66 +10,117 @@ var usuario;
 var voltarMenuPrincipal;
 
 function run(username, funcaoMenuPrincipal) {
-  const caminho = path.join("contas", username + ".json");
+	const caminho = path.join('contas', username + '.json');
 
-  if (fs.existsSync(caminho)) {
-    caminhoUsuario = caminho;
-    usuario = username;
-    voltarMenuPrincipal = funcaoMenuPrincipal;
-    dashboard();
-  } else {
-    throw new Exception("Usuário não existe!");
-  }
+	if (fs.existsSync(caminho)) {
+		caminhoUsuario = caminho;
+		usuario = username;
+		voltarMenuPrincipal = funcaoMenuPrincipal;
+		dashboard();
+	} else {
+		throw new Exception('Usuário não existe!');
+	}
 }
 
 function dashboard() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "opcao",
-        message: "Selecione a opção desejada:",
-        choices: ["Consultar Saldo", "Depositar", "Sacar", "Sair"],
-      },
-    ])
-    .then((respostas) => {
-      const resp = respostas["opcao"];
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'opcao',
+				message: 'Selecione a opção desejada:',
+				choices: ['Consultar Saldo', 'Depositar', 'Sacar', 'Sair'],
+			},
+		])
+		.then((respostas) => {
+			const resp = respostas['opcao'];
 
-      if (resp === "Consultar Saldo") {
-        consultaSaldo();
-      } else if (resp === "Depositar") {
-        deposita();
-      } else if (resp === "Sacar") {
-        saca();
-      } else {
-        voltarMenuPrincipal();
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      voltarMenuPrincipal();
-    });
+			if (resp === 'Consultar Saldo') {
+				consultaSaldo();
+			} else if (resp === 'Depositar') {
+				deposita();
+			} else if (resp === 'Sacar') {
+				saca();
+			} else {
+				voltarMenuPrincipal();
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			voltarMenuPrincipal();
+		});
 }
 
 function consultaSaldo() {
-  console.log("### Consultando saldo ###");
-  //Programe a operação de consulta de saldo aqui
-  /*Não esqueça de invocar a função dashboard() após a execução
-  da operação para o usuário poder continuar operando sua conta; */
+	const contaUsuario = JSON.parse(fs.readFileSync(caminhoUsuario));
+
+	console.log(`Seja bem vindo o seu saldo é : R$${contaUsuario['saldo']}`);
+
+	dashboard();
 }
 
 function deposita() {
-  console.log("### Depositando ###");
-  //Programe a operação de depósito aqui
-  /*Não esqueça de invocar a função dashboard() após a execução
-  da operação para o usuário poder continuar operando sua conta; */
+	inquirer
+		.prompt([
+			{
+				name: 'valor',
+				message: 'Digite o valor que deseja depositar: ',
+			},
+		])
+		.then((resposta) => {
+			const valorDeDeposito = resposta['valor'];
+
+			const dadosConta = JSON.parse(fs.readFileSync(caminhoUsuario));
+
+			const novoSaldo = {
+				type: Number,
+				senha: dadosConta['senha'],
+				saldo: dadosConta['saldo'] + parseFloat(valorDeDeposito),
+			};
+
+			fs.writeFileSync(caminhoUsuario, JSON.stringify(novoSaldo));
+			console.log('Seu deposito foi realizado com sucesso.');
+
+			dashboard();
+		})
+		.catch((err) => {
+			console.log(err);
+			dashboard();
+		});
 }
 
 function saca() {
-  console.log("### Sacando ###");
-  //Programe a operação de saque aqui
-  /*Não esqueça de invocar a função dashboard() após a execução
-  da operação para o usuário poder continuar operando sua conta; */
+	inquirer
+		.prompt([
+			{
+				name: 'saque',
+				message: 'Digite o valor que deseja sacar: ',
+			},
+		])
+		.then((resposta) => {
+			const valorDeSaque = resposta['saque'];
+
+			const dadosAntigos = JSON.parse(fs.readFileSync(caminhoUsuario));
+
+			if (valorDeSaque > dadosAntigos['saldo']) {
+				console.log(
+					'Não foi possível realizar o saque, o saldo é insuficiente.'
+				);
+			} else {
+				const novosDados = {
+					senha: dadosAntigos['senha'],
+					saldo: dadosAntigos['saldo'] - valorDeSaque,
+				};
+
+				fs.writeFileSync(caminhoUsuario, JSON.stringify(novosDados));
+				console.log('Saque foi realizado com sucesso.');
+			}
+			dashboard();
+		})
+		.catch((err) => {
+			console.log(err);
+			dashboard();
+		});
 }
 
 module.exports = run;
